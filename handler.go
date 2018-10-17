680a453dd82f4c6c738c12881de077db6f795b73
haoshuaiwei <haoshuaiwei@jd.com>
2018-10-17 10:52:15 +0800

    fix: handler add network

--- handler.go
+++ handler.go
@@ -20,6 +20,7 @@ import (
 	"github.com/chromedp/cdproto/log"
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/cdproto/runtime"
+	"github.com/chromedp/cdproto/network"
 
 	"github.com/chromedp/chromedp/client"
 )
@@ -42,6 +43,9 @@ type TargetHandler struct {
 
 	// qevents is the incoming event queue.
 	qevents chan *cdproto.Message
+
+	// netevents is for incoming network events
+	netevents chan interface{}
 
 	// detached is closed when the detached event is received.
 	detached chan *inspector.EventDetached
@@ -101,7 +105,7 @@ func (h *TargetHandler) Run(ctxt context.Context) error {
 	for _, a := range []Action{
 		log.Enable(),
 		runtime.Enable(),
-		//network.Enable(),
+		network.Enable(),
 		inspector.Enable(),
 		page.Enable(),
 		dom.Enable(),
@@ -247,7 +251,7 @@ func (h *TargetHandler) processEvent(ctxt context.Context, msg *cdproto.Message)
 	}
 
 	d := msg.Method.Domain()
-	if d != "Page" && d != "DOM" {
+	if d != "Page" && d != "DOM" && d != "Network" {
 		return nil
 	}
 
@@ -259,9 +263,22 @@ func (h *TargetHandler) processEvent(ctxt context.Context, msg *cdproto.Message)
 	case "DOM":
 		h.domWaitGroup.Add(1)
 		go h.domEvent(ctxt, ev)
+
+	case "Network":
+		if h.netevents != nil {
+			h.netevents <- ev
+		}
 	}
 
 	return nil
+}
+
+// NetEvents returns read-only channel for all network events
+func (h *TargetHandler) NetEvents() <-chan interface{} {
+	if h.netevents == nil {
+		h.netevents = make(chan interface{})
+	}
+	return (<-chan interface{})(h.netevents)
 }
 
 // documentUpdated handles the document updated event, retrieving the document
